14af600f08e884c5d1361bf6e8b9c361fb7c35ad
AyaulymAiyp <120308233+AyaulymAiyp@users.noreply.github.com>
2023-06-07 16:46:27 +0600

    real estate

---
+++ v3.php
@@ -0,0 +1,16 @@
+<?php
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+
+$conn = new mysqli('localhost', 'root', '', 'AI AC');
+if($conn->connect_error){
+    die('Connection failed'.$conn->connect_error);
+}else{
+    $stmt = $conn->prepare("insert into cliff_side(name, phone) values(?, ?)");
+    $stmt->bind_param("si", $name, $phone);
+    $stmt->execute();
+    echo 'Thank you for your response, we will contact you in 2-3 work days. <a href="v3.html">Go back</a>';
+    $stmt->close();
+    $conn->close();
+}
+?>
